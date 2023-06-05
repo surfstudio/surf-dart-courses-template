aa38af775a9e8654de3b04bcd9b0c4d5e135f936
DeployChef <dc.titovvlad@gmail.com>
2023-06-05 13:13:28 +0300

    add grass muras

---
+++ lib/grass_murass.dart
@@ -0,0 +1,186 @@
+void main() {
+  Set<AgriculturalMachinery> machines = {};
+
+  mapAfter2010.forEach((key, value) {
+    var allMachines = value.map((e) => e.machineries).reduce((value, element) => element);
+    machines.addAll(allMachines);
+  });
+
+  mapBefore2010.forEach((key, value) {
+    var allMachines = value.map((e) => e.machineries).reduce((value, element) => element);
+    machines.addAll(allMachines);
+  });
+
+  for (var element in machines) {
+    print(element.id + " \t " + element.releaseDate.year.toString());
+  }
+
+  print("\nRESULTS\n");
+
+  final onlyYears = machines.map((e) => e.releaseDate.year).toList();
+  final averageLifetime = _calcAverage(onlyYears);
+  print("Average value of all michines is $averageLifetime");
+
+  onlyYears.sort();
+  final partOldYears = onlyYears.take((onlyYears.length / 2).truncate());
+  final averageLifetimePart = _calcAverage(partOldYears);
+  print("Average value of 50% old michines is $averageLifetimePart");
+}
+
+int _calcAverage(Iterable<int> list) {
+  return (list.reduce((a, b) => a + b) / list.length).truncate();
+}
+
+enum Countries { brazil, russia, turkish, spain, japan }
+
+class Territory {
+  int areaInHectare;
+  List<String> crops;
+  List<AgriculturalMachinery> machineries;
+
+  Territory(
+    this.areaInHectare,
+    this.crops,
+    this.machineries,
+  );
+}
+
+class AgriculturalMachinery {
+  final String id;
+  final DateTime releaseDate;
+
+  AgriculturalMachinery(
+    this.id,
+    this.releaseDate,
+  );
+
+  // Переопределяем оператор "==", что бы сравнивать объекты по значению
+  @override
+  bool operator ==(Object? other) {
+    if (other is! AgriculturalMachinery) return false;
+    if (other.id == id && other.releaseDate == releaseDate) return true;
+
+    return false;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ releaseDate.hashCode;
+}
+
+final mapBefore2010 = <Countries, List<Territory>>{
+  Countries.brazil: [
+    Territory(
+      34,
+      ['Кукуруза'],
+      [
+        AgriculturalMachinery(
+          'Трактор Степан',
+          DateTime(2001),
+        ),
+        AgriculturalMachinery(
+          'Культиватор Сережа',
+          DateTime(2007),
+        ),
+      ],
+    ),
+  ],
+  Countries.russia: [
+    Territory(
+      14,
+      ['Картофель'],
+      [
+        AgriculturalMachinery(
+          'Трактор Гена',
+          DateTime(1993),
+        ),
+        AgriculturalMachinery(
+          'Гранулятор Антон',
+          DateTime(2009),
+        ),
+      ],
+    ),
+    Territory(
+      19,
+      ['Лук'],
+      [
+        AgriculturalMachinery(
+          'Трактор Гена',
+          DateTime(1993),
+        ),
+        AgriculturalMachinery(
+          'Дробилка Маша',
+          DateTime(1990),
+        ),
+      ],
+    ),
+  ],
+  Countries.turkish: [
+    Territory(
+      43,
+      ['Хмель'],
+      [
+        AgriculturalMachinery(
+          'Комбаин Василий',
+          DateTime(1998),
+        ),
+        AgriculturalMachinery(
+          'Сепаратор Марк',
+          DateTime(2005),
+        ),
+      ],
+    ),
+  ],
+};
+
+final mapAfter2010 = {
+  Countries.turkish: [
+    Territory(
+      22,
+      ['Чай'],
+      [
+        AgriculturalMachinery(
+          'Каток Кирилл',
+          DateTime(2018),
+        ),
+        AgriculturalMachinery(
+          'Комбаин Василий',
+          DateTime(1998),
+        ),
+      ],
+    ),
+  ],
+  Countries.japan: [
+    Territory(
+      3,
+      ['Рис'],
+      [
+        AgriculturalMachinery(
+          'Гидравлический молот Лена',
+          DateTime(2014),
+        ),
+      ],
+    ),
+  ],
+  Countries.spain: [
+    Territory(
+      29,
+      ['Арбузы'],
+      [
+        AgriculturalMachinery(
+          'Мини-погрузчик Максим',
+          DateTime(2011),
+        ),
+      ],
+    ),
+    Territory(
+      11,
+      ['Табак'],
+      [
+        AgriculturalMachinery(
+          'Окучник Саша',
+          DateTime(2010),
+        ),
+      ],
+    ),
+  ],
+};
