5cdada7f983a2ed65123048d07a870493cf692d7
michurinnn <michurinnn@gmail.com>
2023-06-07 11:00:34 +0300

    travka-muravka

--- bin/surf_dart_courses_template.dart
+++ bin/surf_dart_courses_template.dart
@@ -1,5 +1,190 @@
-import 'package:surf_dart_courses_template/surf_dart_courses_template.dart' as surf_dart_courses_template;
-
-void main(List<String> arguments) {
-  print('Hello world: ${surf_dart_courses_template.calculate()}!');
+void main() {
+  // Получим список техники из 1-й мапы
+  final List<AgriculturalMachinery> tractorsPartI = mapBefore2010.values
+      .expand(
+        (territories) => territories.expand((tractors) => tractors.machineries),
+      )
+      .toList();
+  // Получим список техники из 2-й мапы
+  final List<AgriculturalMachinery> tractorsPartII = mapAfter2010.values
+      .expand(
+        (territories) => territories.expand((tractors) => tractors.machineries),
+      )
+      .toList();
+  final tractors = tractorsPartI..addAll(tractorsPartII);
+  tractors.sort(
+    (a, b) => a.releaseDate.compareTo(b.releaseDate),
+  );
+  // Все машины, отсортированные по возрасту, без дубликатов
+  final result = tractors.toSet();
+  // Средний возраст машины
+  final averageAge = result.fold<double>(
+      0,
+      (previousValue, element) =>
+          previousValue +
+          DateTime.now().difference(element.releaseDate).inDays);
+  // Средний возраст среди старых машин
+  final oldest = result.toList().sublist(0, result.length ~/ 2).fold(
+      0,
+      (previousValue, element) =>
+          previousValue +
+          DateTime.now().difference(element.releaseDate).inDays);
 }
+
+enum Countries { brazil, russia, turkish, spain, japan }
+
+class Territory {
+  int areaInHectare;
+  List<String> crops;
+  List<AgriculturalMachinery> machineries;
+
+  Territory(
+    this.areaInHectare,
+    this.crops,
+    this.machineries,
+  );
+}
+
+class AgriculturalMachinery {
+  final String id;
+  final DateTime releaseDate;
+
+  AgriculturalMachinery(
+    this.id,
+    this.releaseDate,
+  );
+
+  // Переопределяем оператор "==", что бы сравнивать объекты по значению
+  @override
+  bool operator ==(Object? other) {
+    if (other is! AgriculturalMachinery) return false;
+    if (other.id == id && other.releaseDate == releaseDate) return true;
+
+    return false;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ releaseDate.hashCode;
+  @override
+  String toString() {
+    return '$id ${releaseDate.toString()}';
+  }
+}
+
+final mapBefore2010 = <Countries, List<Territory>>{
+  Countries.brazil: [
+    Territory(
+      34,
+      ['Кукуруза'],
+      [
+        AgriculturalMachinery(
+          'Трактор Степан',
+          DateTime(2001),
+        ),
+        AgriculturalMachinery(
+          'Культиватор Сережа',
+          DateTime(2007),
+        ),
+      ],
+    ),
+  ],
+  Countries.russia: [
+    Territory(
+      14,
+      ['Картофель'],
+      [
+        AgriculturalMachinery(
+          'Трактор Гена',
+          DateTime(1993),
+        ),
+        AgriculturalMachinery(
+          'Гранулятор Антон',
+          DateTime(2009),
+        ),
+      ],
+    ),
+    Territory(
+      19,
+      ['Лук'],
+      [
+        AgriculturalMachinery(
+          'Трактор Гена',
+          DateTime(1993),
+        ),
+        AgriculturalMachinery(
+          'Дробилка Маша',
+          DateTime(1990),
+        ),
+      ],
+    ),
+  ],
+  Countries.turkish: [
+    Territory(
+      43,
+      ['Хмель'],
+      [
+        AgriculturalMachinery(
+          'Комбаин Василий',
+          DateTime(1998),
+        ),
+        AgriculturalMachinery(
+          'Сепаратор Марк',
+          DateTime(2005),
+        ),
+      ],
+    ),
+  ],
+};
+
+final mapAfter2010 = {
+  Countries.turkish: [
+    Territory(
+      22,
+      ['Чай'],
+      [
+        AgriculturalMachinery(
+          'Каток Кирилл',
+          DateTime(2018),
+        ),
+        AgriculturalMachinery(
+          'Комбаин Василий',
+          DateTime(1998),
+        ),
+      ],
+    ),
+  ],
+  Countries.japan: [
+    Territory(
+      3,
+      ['Рис'],
+      [
+        AgriculturalMachinery(
+          'Гидравлический молот Лена',
+          DateTime(2014),
+        ),
+      ],
+    ),
+  ],
+  Countries.spain: [
+    Territory(
+      29,
+      ['Арбузы'],
+      [
+        AgriculturalMachinery(
+          'Мини-погрузчик Максим',
+          DateTime(2011),
+        ),
+      ],
+    ),
+    Territory(
+      11,
+      ['Табак'],
+      [
+        AgriculturalMachinery(
+          'Окучник Саша',
+          DateTime(2010),
+        ),
+      ],
+    ),
+  ],
+};
