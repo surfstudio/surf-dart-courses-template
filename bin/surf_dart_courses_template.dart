fdcf3fcc7d6aa45b3ca1963680c185c5213d111a
Gleb Shalimov <sbgleb10@gmail.com>
2023-09-04 13:44:29 +0300

    Solved task-2

--- bin/surf_dart_courses_template.dart
+++ bin/surf_dart_courses_template.dart
@@ -1,192 +1,82 @@
+class Sneaker {
+  final String brand;
+  final String model;
+  final String color;
+  final double size; // Размер обуви (US)
+  final bool isHighTop; // Высокие или низкие кроссовки
+  final ShoeLace lace; // Шнурки кроссовок
+
+  Sneaker({
+    required this.brand,
+    required this.model,
+    required this.color,
+    required this.size,
+    required this.isHighTop,
+    required this.lace,
+  });
+
+  Sneaker.basic({
+    required this.brand,
+    required this.model,
+  })  : color = "White",
+        size = 10.0,
+        isHighTop = false,
+        lace = ShoeLace.basic();
+
+  void wear() {
+    print("Wearing ${brand} ${model} sneakers...");
+    // Здесь был бы код для надевания кроссовок
+  }
+
+  void tieShoelaces() {
+    print("Tying shoelaces...");
+    lace.tie();
+  }
+}
+
+class ShoeLace {
+  final int length; // Длина шнурков в см
+  final String material; // Материал шнурков
+
+  ShoeLace({
+    required this.length,
+    required this.material,
+  });
+
+  ShoeLace.basic()
+      : length = 120,
+        material = "Nylon";
+
+  void tie() {
+    print("Tying shoelaces...");
+    // Здесь был бы код для завязывания шнурков
+  }
+}
+
 void main() {
-  List<AgriculturalMachinery> allMachineries = [];
-
-  // Собираем все единицы техники в один список
-  mapBefore2010.forEach((country, territories) {
-    territories.forEach((territory) {
-      allMachineries.addAll(territory.machineries);
-    });
-  });
-
-  mapAfter2010.forEach((country, territories) {
-    territories.forEach((territory) {
-      allMachineries.addAll(territory.machineries);
-    });
-  });
-
-  allMachineries.sort((a, b) => a.releaseDate.year - b.releaseDate.year);
-
-  int totalMachineries = allMachineries.length;
-  int halfTotal = (totalMachineries / 2).ceil();
-
-  int sumAllAges = 0;
-  int sumHalfAges = 0;
-
-  for (int i = 0; i < totalMachineries; i++) {
-    int age = DateTime.now().year - allMachineries[i].releaseDate.year;
-    sumAllAges += age;
-
-    if (i < halfTotal) {
-      sumHalfAges += age;
-    }
-  }
-
-  double avgAgeAll = sumAllAges / totalMachineries;
-  double avgAgeHalf = sumHalfAges / halfTotal;
-
-  print('Средний возраст всей техники: $avgAgeAll');
-  print('Средний возраст 50% самой старой техники: $avgAgeHalf');
-}
-
-enum Countries { brazil, russia, turkish, spain, japan }
-
-class Territory {
-  int areaInHectare;
-  List<String> crops;
-  List<AgriculturalMachinery> machineries;
-
-  Territory(
-    this.areaInHectare,
-    this.crops,
-    this.machineries,
-  );
-}
-
-class AgriculturalMachinery {
-  final String id;
-  final DateTime releaseDate;
-
-  AgriculturalMachinery(
-    this.id,
-    this.releaseDate,
+  // Создание кроссовок с помощью основного конструктора
+  Sneaker mySneakers = Sneaker(
+    brand: "Nike",
+    model: "Air Max 90",
+    color: "Black/White",
+    size: 9.5,
+    isHighTop: false,
+    lace: ShoeLace(
+      length: 150,
+      material: "Polyester",
+    ),
   );
 
-  @override
-  bool operator ==(Object? other) {
-    if (other is! AgriculturalMachinery) return false;
-    if (other.id == id && other.releaseDate == releaseDate) return true;
+  // Создание кроссовок с помощью альтернативного конструктора
+  Sneaker basicSneakers = Sneaker.basic(
+    brand: "Adidas",
+    model: "Superstar",
+  );
 
-    return false;
-  }
+  // Использование методов кроссовок
+  mySneakers.wear();
+  mySneakers.tieShoelaces();
 
-  @override
-  int get hashCode => id.hashCode ^ releaseDate.hashCode;
+  // Использование вложенного объекта
+  print("My shoelaces are made of ${mySneakers.lace.material}.");
 }
-
-final mapBefore2010 = <Countries, List<Territory>>{
-  Countries.brazil: [
-    Territory(
-      34,
-      ['Кукуруза'],
-      [
-        AgriculturalMachinery(
-          'Трактор Степан',
-          DateTime(2001),
-        ),
-        AgriculturalMachinery(
-          'Культиватор Сережа',
-          DateTime(2007),
-        ),
-      ],
-    ),
-  ],
-  Countries.russia: [
-    Territory(
-      14,
-      ['Картофель'],
-      [
-        AgriculturalMachinery(
-          'Трактор Гена',
-          DateTime(1993),
-        ),
-        AgriculturalMachinery(
-          'Гранулятор Антон',
-          DateTime(2009),
-        ),
-      ],
-    ),
-    Territory(
-      19,
-      ['Лук'],
-      [
-        AgriculturalMachinery(
-          'Трактор Гена',
-          DateTime(1993),
-        ),
-        AgriculturalMachinery(
-          'Дробилка Маша',
-          DateTime(1990),
-        ),
-      ],
-    ),
-  ],
-  Countries.turkish: [
-    Territory(
-      43,
-      ['Хмель'],
-      [
-        AgriculturalMachinery(
-          'Комбаин Василий',
-          DateTime(1998),
-        ),
-        AgriculturalMachinery(
-          'Сепаратор Марк',
-          DateTime(2005),
-        ),
-      ],
-    ),
-  ],
-};
-
-final mapAfter2010 = {
-  Countries.turkish: [
-    Territory(
-      22,
-      ['Чай'],
-      [
-        AgriculturalMachinery(
-          'Каток Кирилл',
-          DateTime(2018),
-        ),
-        AgriculturalMachinery(
-          'Комбаин Василий',
-          DateTime(1998),
-        ),
-      ],
-    ),
-  ],
-  Countries.japan: [
-    Territory(
-      3,
-      ['Рис'],
-      [
-        AgriculturalMachinery(
-          'Гидравлический молот Лена',
-          DateTime(2014),
-        ),
-      ],
-    ),
-  ],
-  Countries.spain: [
-    Territory(
-      29,
-      ['Арбузы'],
-      [
-        AgriculturalMachinery(
-          'Мини-погрузчик Максим',
-          DateTime(2011),
-        ),
-      ],
-    ),
-    Territory(
-      11,
-      ['Табак'],
-      [
-        AgriculturalMachinery(
-          'Окучник Саша',
-          DateTime(2010),
-        ),
-      ],
-    ),
-  ],
-};
