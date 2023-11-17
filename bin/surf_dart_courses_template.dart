571c8aff446085a1b76d8a08c9408834187763f2
halxian <magistralbert@gmail.com>
2023-11-17 16:13:07 +0300

    task-7

--- bin/surf_dart_courses_template.dart
+++ bin/surf_dart_courses_template.dart
@@ -1,177 +1,4 @@
-final listProduct = [
-  RawProductItem(
-    name: 'Персик',
-    categoryName: 'Растительная пища',
-    subcategoryName: 'Фрукты',
-    expirationDate: DateTime(2023, 12, 22),
-    qty: 5,
-  ),
-  RawProductItem(
-    name: 'Молоко',
-    categoryName: 'Молочные продукты',
-    subcategoryName: 'Напитки',
-    expirationDate: DateTime(2022, 12, 22),
-    qty: 5,
-  ),
-  RawProductItem(
-    name: 'Кефир',
-    categoryName: 'Молочные продукты',
-    subcategoryName: 'Напитки',
-    expirationDate: DateTime(2022, 12, 22),
-    qty: 5,
-  ),
-  RawProductItem(
-    name: 'Творог',
-    categoryName: 'Молочные продукты',
-    subcategoryName: 'Не напитки',
-    expirationDate: DateTime(2022, 12, 22),
-    qty: 0,
-  ),
-  RawProductItem(
-    name: 'Творожок',
-    categoryName: 'Молочные продукты',
-    subcategoryName: 'Не напитки',
-    expirationDate: DateTime(2022, 12, 22),
-    qty: 0,
-  ),
-  RawProductItem(
-    name: 'Творог',
-    categoryName: 'Молочные продукты',
-    subcategoryName: 'Не напитки',
-    expirationDate: DateTime(2022, 12, 22),
-    qty: 0,
-  ),
-  RawProductItem(
-    name: 'Гауда',
-    categoryName: 'Молочные продукты',
-    subcategoryName: 'Сыры',
-    expirationDate: DateTime(2022, 12, 22),
-    qty: 3,
-  ),
-  RawProductItem(
-    name: 'Маасдам',
-    categoryName: 'Молочные продукты',
-    subcategoryName: 'Сыры',
-    expirationDate: DateTime(2022, 12, 22),
-    qty: 2,
-  ),
-  RawProductItem(
-    name: 'Яблоко',
-    categoryName: 'Растительная пища',
-    subcategoryName: 'Фрукты',
-    expirationDate: DateTime(2022, 12, 4),
-    qty: 4,
-  ),
-  RawProductItem(
-    name: 'Морковь',
-    categoryName: 'Растительная пища',
-    subcategoryName: 'Овощи',
-    expirationDate: DateTime(2022, 12, 23),
-    qty: 51,
-  ),
-  RawProductItem(
-    name: 'Черника',
-    categoryName: 'Растительная пища',
-    subcategoryName: 'Ягоды',
-    expirationDate: DateTime(2022, 12, 25),
-    qty: 0,
-  ),
-  RawProductItem(
-    name: 'Курица',
-    categoryName: 'Мясо',
-    subcategoryName: 'Птица',
-    expirationDate: DateTime(2022, 12, 18),
-    qty: 2,
-  ),
-  RawProductItem(
-    name: 'Говядина',
-    categoryName: 'Мясо',
-    subcategoryName: 'Не птица',
-    expirationDate: DateTime(2022, 12, 17),
-    qty: 0,
-  ),
-  RawProductItem(
-    name: 'Телятина',
-    categoryName: 'Мясо',
-    subcategoryName: 'Не птица',
-    expirationDate: DateTime(2022, 12, 17),
-    qty: 0,
-  ),
-  RawProductItem(
-    name: 'Индюшатина',
-    categoryName: 'Мясо',
-    subcategoryName: 'Птица',
-    expirationDate: DateTime(2022, 12, 17),
-    qty: 0,
-  ),
-  RawProductItem(
-    name: 'Утка',
-    categoryName: 'Мясо',
-    subcategoryName: 'Птица',
-    expirationDate: DateTime(2022, 12, 18),
-    qty: 2,
-  ),
-  RawProductItem(
-    name: 'Гречка',
-    categoryName: 'Растительная пища',
-    subcategoryName: 'Крупы',
-    expirationDate: DateTime(2022, 12, 22),
-    qty: 8,
-  ),
-  RawProductItem(
-    name: 'Свинина',
-    categoryName: 'Мясо',
-    subcategoryName: 'Не птица',
-    expirationDate: DateTime(2022, 12, 23),
-    qty: 5,
-  ),
-  RawProductItem(
-    name: 'Груша',
-    categoryName: 'Растительная пища',
-    subcategoryName: 'Фрукты',
-    expirationDate: DateTime(2022, 12, 25),
-    qty: 5,
-  ),
-];
-
-void main(List<String> arguments) {
-  sort(listProduct);
-}
-
-void sort(List<RawProductItem> products) {
-  final productsStock = filterProducts(products);
-  final productCategoryName =
-      productsStock.map<String>((e) => e.categoryName).toSet().toList();
-  final productsubcategoryName =
-      productsStock.map<String>((e) => e.subcategoryName).toSet().toList();
-
-  for (var category in productCategoryName) {
-    var productIteration = productsStock
-        .where((product) =>
-            category == product.categoryName && category.isNotEmpty)
-        .toSet()
-        .toList();
-    for (var subcategory in productsubcategoryName) {
-      print(productIteration
-          .where((product) => subcategory == product.subcategoryName)
-          .toSet()
-          .toList()
-          .map<Map<String, Map<String, List<String>>>>((e) => {
-                e.categoryName: {
-                  e.subcategoryName: [e.name]
-                }
-              })
-          .toSet()
-          .toList());
-    }
-  }
-}
-
-List<RawProductItem> filterProducts(List<RawProductItem> products) => products
-    .where((element) =>
-        element.expirationDate.isAfter(DateTime(2021, 12, 21)) &&
-        element.qty > 0)
-    .toList();
+import 'moks.dart';
 
 class RawProductItem {
   final String name;
@@ -180,16 +7,32 @@ class RawProductItem {
   final DateTime expirationDate;
   final int qty;
 
-  const RawProductItem({
-    required this.name,
-    required this.categoryName,
-    required this.subcategoryName,
-    required this.expirationDate,
-    required this.qty,
-  });
+  RawProductItem(
+      {required this.name,
+      required this.categoryName,
+      required this.subcategoryName,
+      required this.expirationDate,
+      required this.qty});
+}
 
-  @override
-  String toString() {
-    return name;
+Map<String, Map<String, List<String>>> moks(List<RawProductItem> items) {
+  final result = <String, Map<String, List<String>>>{};
+  final now = DateTime.now();
+  for (final item in items) {
+    if (item.qty == 0 || now.isAfter(item.expirationDate)) {
+      continue;
+    }
+    if (!result.containsKey(item.categoryName)) {
+      result[item.categoryName] = {};
+    }
+    if (!result[item.categoryName]!.containsKey(item.subcategoryName)) {
+      result[item.categoryName]![item.subcategoryName] = [];
+    }
+    result[item.categoryName]![item.subcategoryName]!.add(item.name);
   }
+  return result;
+}
+
+void main() {
+  print(moks(moks1));
 }
